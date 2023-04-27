ee932f7049b8fad5fdf47686f76eefad684a7b81
Michael <michael@colorado4x4.net>
2023-04-27 14:35:43 -0600

    I want to be done

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,12 @@
 // src/hooks.server.ts
-import {
-    PUBLIC_SUPABASE_URL,
-    PUBLIC_SUPABASE_ANON_KEY
-  } from '$env/static/public';
+// import {
+//     PUBLIC_SUPABASE_URL,
+//     PUBLIC_SUPABASE_ANON_KEY
+//   } from '$env/static/public';
   import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
   import type { Handle, error } from '@sveltejs/kit';
-  
+  const PUBLIC_SUPABASE_URL = "https://qycqbcxdzugudihdfmfk.supabase.co"
+  const PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Y3FiY3hkenVndWRpaGRmbWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcxOTY2MTEsImV4cCI6MTk5Mjc3MjYxMX0.d2VeKHq6_GIscI6rGhwUFetpwXPl8Vp3hNJptMuaeFI"
   export async function handle({ event, resolve }) {
     event.locals.supabase = createSupabaseServerClient({
       supabaseUrl: PUBLIC_SUPABASE_URL,
