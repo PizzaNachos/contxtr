90aef2eb16cb55333d778f7f5e21cd545c852a9f
Michael <michael@colorado4x4.net>
2023-04-29 22:24:56 -0600

    Fixing stuff

--- src/lib/supabase_client.ts
+++ src/lib/supabase_client.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/dynamic/public'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-let url = "https://qycqbcxdzugudihdfmfk.supabase.co"
-let key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Y3FiY3hkenVndWRpaGRmbWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcxOTY2MTEsImV4cCI6MTk5Mjc3MjYxMX0.d2VeKHq6_GIscI6rGhwUFetpwXPl8Vp3hNJptMuaeFI"
-
-export const supabase = createClient(url, key)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
